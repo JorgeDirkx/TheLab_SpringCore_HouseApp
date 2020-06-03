bc1d85eaccccaf2d61b2ee676395b07670cbcd74
JorgeDirkx <jorge.dirkx@gmail.com>
2020-06-03 22:58:00 +0200

    comments added;2

--- src/main/java/com/jorgedirkx/spring/springcore/tools/implementations/LawnMower.java
+++ src/main/java/com/jorgedirkx/spring/springcore/tools/implementations/LawnMower.java
@@ -5,7 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+//communicating with Spring; make a bean out of this class. Scan for components in HouseConfiguration ?
+//add @ComponentScan in public class Houseconfiguration OR use @bean as stated in HouseConfiguration.
 @Component
+//
 @Primary
 @Qualifier("outsideTool")
 public class LawnMower implements GardeningTool {
